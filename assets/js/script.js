475a084cd446d579696cea76400c266bafd83797
Diezte <83254494+Diezte@users.noreply.github.com>
2021-06-10 22:05:21 -0300

    Se sumo Array de objetos, y se crearon las card desde JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,96 +1,82 @@
 
-function saludar(){
+let productos = [
+    {
+        id:0,
+        marca: "fender",
+        modelo: "Telecaster",
+        anio: 1980,
+        precio: 98000,
+        imagen:"assets/img/productos/fender-telecaster.jpg",
+        filtro: "filter-electricas"
+    },
+    {
+        id:1,
+        marca: "fender",
+        modelo: "malibu",
+        anio: 1996,
+        precio: 95000,
+        imagen:"assets/img/productos/fender-malibu.jpg",
+        filtro: "filter-acusticas"
+    },
 
-    alert("Bienvenido a Artico Musica, no olvides de registrarte para recibir todas nuestras novedades")
-}
-class Procucto{
-    
-    constructor(marca,modelo,anio,precio,stock){
-
-        this.marca = marca.toUpperCase();
-        this.modelo = modelo.toUpperCase();
-        this.anio = anio;
-        this.precio = parseFloat(precio);
-        this.stock = true;
+    {
+        id:2,
+        marca: "taylor",
+        modelo: "D400",
+        anio: 2005,
+        precio: 130500,
+        imagen:"assets/img/productos/taylor-412electroacustic.jpg",
+        filtro: "filter-electroAcusticas"
+    },
+    {
+        id:3,
+        marca: "gibson",
+        modelo: "SG",
+        anio: 1975,
+        precio: 120000,
+        imagen:"assets/img/productos/gibson-sg.jpg",
+        filtro: "filter-electricas"
+    },
+    {
+        id:4,
+        marca: "gibson",
+        modelo: "lespaul",
+        anio: 1996,
+        precio: 105000,
+        imagen:"assets/img/productos/gibson-Lespaul.jpg",
+        filtro: "filter-electricas"
+    },
+    {
+        id:5,
+        marca: "fender",
+        modelo: "Stratocaster",
+        anio: 1975,
+        precio: 135000,
+        imagen:"assets/img/productos/fender-stratocaster.jpg",
+        filtro: "filter-electricas"
     }
+]
 
-    preciofinal (){
-        this.precio = this.precio * 1.21;
-        }
+const listaProductos = document.getElementById('productos')
 
-    sinstock (){
+productos.forEach(prod => {
+    let card = document.createElement('div')
+    card.innerHTML = `
+        
+        <div class="col-lg-4 col-md-6 portfolio-item">
+            <div class="portfolio-wrap">
+                <img src= ${prod.imagen} class="img-fluid" alt="">
+                <div class="portfolio-links">
+                <a class="ver" title="Detalles"><i class="ver">ver</i></i></a>
+                <a id="prod-${prod.id}" class="comprar" title="Detalles"><i class="ver">Comprar</i></i></a>
+                </div>
+            </div>
+        </div>
+    `
+    listaProductos.appendChild(card)
 
-        this.stock = false;
-    }
-
-    mostrarGuitarra() {
-
-        var mostar =("Marca: " + this.marca + " Modelo: " + this.modelo + " Fabricada en: " + this.anio + " Su precio es: " + this.precio)
-        return mostar;
-    }
-
-    mostrarNombre(){
-        var mostrarNombre=(this.modelo);
-        return mostrarNombre;
-    }
-
-}
-
-
-
-const guitarraUno = new Procucto("fender","Telecaster",1980,98000);
-const guitarraDos = new Procucto("fender","malibu",1996,95000);
-const guitarraTres = new Procucto("Taylor","D400", 2005, 130500);
-const guitarraCuatro = new Procucto("Gibson","SG",1975,120000);
-const guitarraCinco = new Procucto("Gibson","LesPaul", 1996, 100500);
-const guitarraSeis = new Procucto("fender","stratocaster",1975,128000);
-const guitarraSiete = new Procucto("epiphone", "AJ-100", 1965, 221000);
-const guitarraOcho = new Procucto("epiphone", "AJ-100", 1965, 221000);
-const guitarraNueve = new Procucto("epiphone", "AJ-100", 1965, 221000);
-
-
-const arrayGuitarras = [guitarraUno,guitarraDos,guitarraTres,guitarraCuatro,guitarraCinco,guitarraSeis,guitarraSiete,guitarraOcho,guitarraNueve];
-
-const comprar = document.getElementsByClassName("comprar");
-const especificaciones = document.getElementById("especificaciones");
-const nombresMarcas = document.getElementById("exampleModalLabel");
-
-
-
-    
-comprar[0].addEventListener ("click", () => {
-
-    especificaciones.innerText = arrayGuitarras[0].mostrarGuitarra();
-    nombresMarcas.innerText = arrayGuitarras[0].mostrarNombre();
-});
-
-comprar[1].addEventListener ("click", () => {
-
-    especificaciones.innerText = arrayGuitarras[1].mostrarGuitarra();
-    nombresMarcas.innerText = arrayGuitarras[1].mostrarNombre();
-});
-
-
-comprar[2].addEventListener ("click", () => {
-
-    especificaciones.innerText = arrayGuitarras[2].mostrarGuitarra();
-
-});
-
-comprar[3].addEventListener ("click", () => {
-
-    especificaciones.innerText = arrayGuitarras[3].mostrarGuitarra();
-
-});
-
-comprar[4].addEventListener ("click", () => {
-
-    especificaciones.innerText = arrayGuitarras[4].mostrarGuitarra();
-
-});
-
-comprar[5].addEventListener ("click", () => {
-
-    especificaciones.innerText = arrayGuitarras[5].mostrarGuitarra();
-
-});
+    let boton = document.getElementById('prod-' + prod.id)
+    boton.addEventListener("click", () =>{
+        console.log("se agrego al carrito el producto ID:" + prod.id)
+    } )
+})
