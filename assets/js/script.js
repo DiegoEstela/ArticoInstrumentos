a0943b723cb292f267f6a454daea67f29ae62424
Diego Ezequiel Estela <83254494+Diezte@users.noreply.github.com>
2021-06-30 19:29:49 -0300

    Se agrego una tabla para mostrar el carrito

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,8 +8,31 @@ function agregarItem(objeto){
 
     Object.defineProperty(objeto, 'cant', {value: 1, writable: true});
     carrito.push(objeto);
+    console.log(carrito);
+
+    carrito.forEach(prod => {
+
+
+        $("#cuerpoTabla").append(`
+    
+        <tr>
+        <th scope="row">${prod.id}</th>
+        <td>${prod.cant}</td>
+        <td>${prod.marca}</td>
+        <td>${prod.modelo}</td>
+        <td>${valorDolar}</td>
+        <td>${prod.precio}</td>
+        <td>${prod.precio*2}</td>
+        </tr>
+        `
+        )
+        
+    })
+
+    
 
 }
+
 
 
 
@@ -43,16 +66,43 @@ productos.forEach(prod => {
         botonAgregar.addEventListener("click", () =>{ 
             numeroCompras.push(+1)
             $("#bntCarrito").append(`<span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger"> ${numeroCompras.length} </span>`)
-            carrito.push(productos[prod.id - 1])
-            console.log(carrito);
-
-            carrito.forEach (prod => {
-                
-                $("#cuerpoCarrito").append(+ prod.marca + " " + prod.modelo);
-            })
             
-        })
-        
+            let idEncontrado = false;
+            for (item of carrito){
+                if (item.id == prod.id){
+                    console.log("existe id igual");
+                    item.cant = item.cant + 1;
+                    console.log(carrito);
+                    idEncontrado = true;
     
+                }
+            }
+            if(idEncontrado == false){
+                objetoAAgregar = productos [prod.id - 1];
+                agregarItem(objetoAAgregar);
+            }
+
+            })
+         
+    
+
+})
+
+carrito.forEach(prod => {
+
+    $("#cuerpoTabla").append(`
+
+    
+    <tr>
+    <th scope="row">${prod.id}</th>
+    <td>${prod.cant}</td>
+    <td>${prod.marca}</td>
+    <td>${prod.modelo}</td>
+    <td>${valorDolar}</td>
+    <td>${prod.precio}</td>
+    <td>${prod.precio*2}</td>
+    </tr>
+    `
+    )
 
 })
